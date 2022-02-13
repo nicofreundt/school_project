325fed94ffce200df79cb63af7fa33ae270b810c
Lukas Krähling <lukas.k98@web.de>
2022-02-13 23:27:40 +0100

    Created first DB-Models

--- onboarding/models.py
+++ onboarding/models.py
@@ -1,3 +1,44 @@
-from django.db import models
+from django.db.models import (
+    CharField,
+    IntegerField,
+    BooleanField,
+    ForeignKey,
+    Model,
+    CASCADE,
+)
 
 # Create your models here.
+class Creator(Model):
+    name = CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class LoginID(Model):
+    loginID = IntegerField()
+    group = BooleanField()
+
+
+class Survey(Model):
+    title = CharField(max_length=100)
+    loginID = ForeignKey(LoginID, default=0, on_delete=CASCADE)
+    creator = ForeignKey(Creator, on_delete=CASCADE)
+    closed = BooleanField()
+
+    def __str__(self):
+        return self.title
+
+
+class Participant(Model):
+    loginID = ForeignKey(LoginID, default=0, on_delete=CASCADE)
+
+
+class Question(Model):
+    survey = ForeignKey(Survey, on_delete=CASCADE)
+    text = CharField(max_length=250)
+
+
+class Answer(Model):
+    quaetion = ForeignKey(Question, on_delete=CASCADE)
+    rating = IntegerField()
