c739b1bd87e3ce28375ebbffcc5f4b752089b097
Lukas Krähling <lukas.k98@web.de>
2022-02-13 23:52:26 +0100

    Added connection to MariaDB

--- feedbackDjango/settings.py
+++ feedbackDjango/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "feedbackDjango.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "feedback",
+        "USER": "root",
+        "PASSWORD": "123",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
