a0ea2eda8a425222b4c1baba8fb85829593c40c3
Lukas Krähling <lukas.k98@web.de>
2022-02-15 22:21:17 +0100

    Finished renaming

--- feedback_django/asgi.py
+++ feedback_django/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for feedbackDjango project.
+ASGI config for feedback_django project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feedbackDjango.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "feedback_django.settings")
 
 application = get_asgi_application()
