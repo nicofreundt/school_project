1474140a97cbe3db79ecca30b25f62a314446723
Lukas Krähling <lukas.k98@web.de>
2022-02-15 22:14:47 +0100

    Deleted old apps which dont fit in new structure

--- onboarding/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("creator/", views.onboardCreator, name="onboardCreator"),
-    path("participant/", views.onboardParticipant, name="onboardParticipant"),
-]
