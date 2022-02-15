5f0e54dd98e723a3e4323e6253ca199c9146acd2
Lukas Krähling <lukas.k98@web.de>
2022-02-15 22:42:03 +0100

    Deleted overlooked & unneeded app

--- creator/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def signIn(response):
-    return HttpResponse("<h1>Username:</h1><br><h1>Password:</h1>")
