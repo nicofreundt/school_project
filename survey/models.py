9a48776c195f07a470c1b501909ef81db5123db2
Lukas Kraehling <lukas.K98@web.de>
2022-02-16 13:00:20 +0100

    Streamlined models

--- survey/models.py
+++ survey/models.py
@@ -1,29 +1,36 @@
 from django.db.models import (
     CharField,
+    TextField,
     IntegerField,
     BooleanField,
     ForeignKey,
+    UUIDField,
     Model,
     CASCADE,
 )
-from user_management.models import LoginID, Creator
+from uuid import uuid4
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# Own Models
+from user_management.models import Creator
 
 # Create your models here.
 class Survey(Model):
+    id = UUIDField(primary_key=True, default=uuid4, editable=False)
     title = CharField(max_length=100)
-    loginID = ForeignKey(LoginID, default="0000000000", on_delete=CASCADE)
+    description = TextField()
     creator = ForeignKey(Creator, on_delete=CASCADE)
     closed = BooleanField()
-
-    def __str__(self):
-        return self.title
 
 
 class Question(Model):
     survey = ForeignKey(Survey, on_delete=CASCADE)
-    text = CharField(max_length=250)
+    position = IntegerField()
+    text = TextField()
 
 
 class Answer(Model):
     question = ForeignKey(Question, on_delete=CASCADE)
-    rating = IntegerField()
+    rating = IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(5)],
+    )
--- user_management/models.py
+++ user_management/models.py
@@ -1,24 +1,12 @@
 from django.db.models import (
     CharField,
-    BooleanField,
-    ForeignKey,
     Model,
-    CASCADE,
 )
-from django.core.validators import MinLengthValidator
+from django.forms import EmailField
 
 ### MODELS ####
 class Creator(Model):
-    name = CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
-class LoginID(Model):
-    loginID = CharField(max_length=10, validators=[MinLengthValidator(4)])
-    group = BooleanField()
-
-
-class Participant(Model):
-    loginID = ForeignKey(LoginID, default=0, on_delete=CASCADE)
+    first_name = CharField(max_length=50)
+    last_name = CharField(max_length=50)
+    email = EmailField()
+    # LDAP
