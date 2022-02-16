694561578e46fc5f404b4992d86cb0a53107c153
Lukas Kraehling <lukas.K98@web.de>
2022-02-16 14:41:18 +0100

    Added GET & POST URL-Patterns

--- survey/urls.py
+++ survey/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.survey_overview, name="Survey Overview"),
+    path("create/", views.create_survey.as_view(), name="Create Survey"),
+    path("create/", views.survey_overview, name="Create Survey"),
 ]
