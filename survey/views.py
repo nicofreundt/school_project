6ef5c922de6fdb34d899b5482bf237b474de65d0
Lukas Kraehling <lukas.K98@web.de>
2022-02-16 14:40:28 +0100

    Added create_survey View-Class and SurveyForm-Class

--- survey/views.py
+++ survey/views.py
@@ -1,5 +1,30 @@
+from django.forms import ModelForm
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.views import View
+
+from survey.models import Survey
+from user_management.models import Creator
 
 # Create your views here.
 def survey_overview(response):
-    return HttpResponse("SURVEY")
+    return HttpResponse(Survey.objects.all())
+
+
+class create_survey(View):
+    def get(self, request, id=None, *args, **kwargs):
+        return render(request, "create_survey.html", {"form": SurveyForm()})
+
+    def post(self, request, id=None, *args, **kwargs):
+        Survey(
+            title=request.POST.get("title"),
+            description=request.POST.get("description"),
+            creator=Creator.objects.get(pk=1),
+        ).save()
+        return redirect("/survey/")
+
+
+class SurveyForm(ModelForm):
+    class Meta:
+        model = Survey
+        fields = ["title", "description"]
