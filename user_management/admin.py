49773348c5e97d7bdc380dc928852a7fe77fb253
Lukas Kraehling <lukas.K98@web.de>
2022-02-16 14:02:36 +0100

    Added admin config

--- user_management/admin.py
+++ user_management/admin.py
@@ -1,3 +1,8 @@
+from re import A
 from django.contrib import admin
+from .models import Creator
+from survey.models import Survey, Question, Answer
 
 # Register your models here.
+admin.site.register(Creator)
+admin.site.register([Survey, Question, Answer])
