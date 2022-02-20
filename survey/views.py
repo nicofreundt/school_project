f26da9e85956309f7edcb9b996bcfc82008c5517
Lukas Krähling <lukas.k98@web.de>
2022-02-20 23:24:51 +0100

    survey_overview() returns detailed Survey-Info now
     - Modified reditect of create_survey-View to match new Path in urls.py

--- survey/views.py
+++ survey/views.py
@@ -2,14 +2,20 @@ from django.forms import ModelForm
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.views import View
-from django.conf import settings
+from django.contrib.auth.models import User
+from django.db.models.functions import Lower
 
 from survey.models import Survey
-from django.contrib.auth.models import User
 
 # Create your views here.
 def survey_overview(response):
-    return HttpResponse(Survey.objects.all())
+    surveysHTML = "<ul>"
+    # "-title" means DESC
+    for survey in Survey.objects.all().order_by("title").iterator():
+        surveysHTML += "<li>" + str(survey) + "</li>"
+    surveysHTML += "</ul>"
+
+    return HttpResponse(surveysHTML)
 
 
 class create_survey(View):
@@ -22,7 +28,7 @@ class create_survey(View):
             description=request.POST.get("description"),
             creator=User.objects.get(pk=1),
         ).save()
-        return redirect("/survey/")
+        return redirect("/overview")
 
 
 class SurveyForm(ModelForm):
