cceb159e45b0e397a883c8c81f3136e718a1ecf1
Lukas Krähling <lukas.k98@web.de>
2022-02-20 23:19:38 +0100

    Implemented redirect: survey/ -> survey/overview/

--- survey/urls.py
+++ survey/urls.py
@@ -1,8 +1,9 @@
+from django.shortcuts import redirect
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.survey_overview, name="Survey Overview"),
+    path("", lambda request: redirect("overview/", permanent=True)),
+    path("overview/", views.survey_overview, name="Survey Overview"),
     path("create/", views.create_survey.as_view(), name="Create Survey"),
-    path("create/", views.survey_overview, name="Create Survey"),
 ]
