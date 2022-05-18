70d357ee64d06c4ed12979923195d11f83fe757f
Lukas Krähling <l.kraehling@protonmail.com>
2022-05-18 12:01:45 +0200

    Modified survey_delete URL

--- survey/urls.py
+++ survey/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", lambda request: redirect("survey_overview", permanent=True)),
     path("overview/", views.survey_overview, name="survey_overview"),
     path("create/", views.survey_create.as_view(), name="survey_create"),
-    path("delete/<survey>/", views.survey_delete.as_view(), name="survey_delete"),
+    path("delete/<survey>/", views.survey_delete, name="survey_delete"),
     path("edit/<survey>/", views.survey_edit.as_view(), name="survey_edit"),
     path(
         "question/<question>/",
