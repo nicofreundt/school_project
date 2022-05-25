8072d4a238242af1656664c3ffa421bfdd1d0bb3
Lukas Krähling <l.kraehling@protonmail.com>
2022-05-25 12:52:35 +0200

    Implemented URL-path to add questions to surveys
    - Improved questions-edit url-path

--- survey/urls.py
+++ survey/urls.py
@@ -9,7 +9,12 @@ urlpatterns = [
     path("delete/<survey>/", views.survey_delete, name="survey_delete"),
     path("edit/<survey>/", views.survey_edit.as_view(), name="survey_edit"),
     path(
-        "question/<question>/",
+        "edit/<survey>/create/question/",
+        views.question_create.as_view(),
+        name="question_create",
+    ),
+    path(
+        "edit/<survey>/edit/question/<question>/",
         views.question_edit.as_view(),
         name="question_edit",
     ),
